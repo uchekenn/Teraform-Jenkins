d70d84c23e50b1aedbc1fe5c597eaabacfa6b4d0
uchekenn <68679009+uchekenn@users.noreply.github.com>
2023-02-17 00:14:17 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,88 +1,30 @@
 provider "aws" {
-  region  = "us-east-1"
-}
+	  profile = "default"
+	  region  = "us-east-1"
+	}
 
-variable "ingressrules" {
-  type    = list(number)
-  default = [80, 443, 22]
-}
+	data "aws_ami" "ubuntu" {
+	  most_recent = true
 
-resource "aws_security_group" "web_traffic" {
-  name        = "Allow web traffic"
-  description = "Allow ssh and standard http/https ports inbound and everything outbound"
+	  filter {
+	    name   = "name"
+	    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+	  }
 
-  dynamic "ingress" {
-    iterator = port
-    for_each = var.ingressrules
-    content {
-      from_port   = port.value
-      to_port     = port.value
-      protocol    = "TCP"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
+	  filter {
+	    name   = "virtualization-type"
+	    values = ["hvm"]
+	  }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+	  owners = ["099720109477"]
+	}
 
-  tags = {
-    "Terraform" = "true"
-  }
-}
+	resource "aws_instance" "jenkins" {
+	  ami           = data.aws_ami.ubuntu.id
+	  instance_type = "t2.micro"
 
-data "aws_ami" "ubuntu" {
-most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-
-
-resource "aws_instance" "jenkins" {
-  ami             = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  security_groups = [aws_security_group.web_traffic.name]
-  key_name        = "kluu"
-
-  provisioner "remote-exec" {
-    inline = [
-      "wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -",
-      "sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'",
-      "sudo apt update -qq",
-      "sudo apt install -y default-jre",
-      "sudo apt install -y jenkins",
-      "sudo systemctl start jenkins",
-      "sudo iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080",
-      "sudo sh -c \"iptables-save > /etc/iptables.rules\"",
-      "echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections",
-      "echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections",
-      "sudo apt-get -y install iptables-persistent",
-      "sudo ufw allow 8080",
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    host        = self.public_ip
-    user        = "ubuntu"
-    private_key = file("~/kluu.pem")
-  }
-
-  tags = {
-    "Name"      = "Jenkins_Server"
-    "Terraform" = "true"
-  }
-}
+	  tags = {
+	    "Name"      = "Jenkins_Server"
+	    "Terraform" = "true"
+	  }
+	}
